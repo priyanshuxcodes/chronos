b58dfd714642bb0c7db9eb72035423a84da423f8
priyanshuxcodes <priyanshusharma8988@gmail.com>
2026-06-18 23:17:35 +0530

    Done

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,20 @@
-export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/ai/:path*"],
 };
